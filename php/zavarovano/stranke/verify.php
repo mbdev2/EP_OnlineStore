fe14b8389630ec9a88d51c6af0b267963e1e21c0
mbdev2 <mark.breznik@gmail.com>
2020-12-17 11:04:49 +0100

    Narejena prijava z mailom

--- php/zavarovano/stranke/verify.php
+++ php/zavarovano/stranke/verify.php
@@ -5,8 +5,6 @@ if(isset($_GET['email']) && !empty($_GET['email']) AND isset($_GET['hash']) && !
     $emailUp = $_GET['email'];
     $registerHash = $_GET['hash'];
 
-    echo 'Opcija 1';
-
     $queryAction = mysqli_prepare($dbConnection, "SELECT * FROM stranke WHERE eNaslov = ? AND registerHash = ? LIMIT 1");
 		mysqli_stmt_bind_param($queryAction, 'ss', $emailUp, $registerHash);
 		mysqli_stmt_execute($queryAction);
@@ -15,26 +13,27 @@ if(isset($_GET['email']) && !empty($_GET['email']) AND isset($_GET['hash']) && !
 
     if(isset($curUser)){
 			if($curUser['activeOrNot']==1){
-        echo 'Opcija 45';
-        //header("Location: ../skupno/prijava.php");
+        echo '<script>alert("Racun je ze registriran")</script>';
+        header("Location: ../skupno/prijava.php");
 			}
       else{
         $query = mysqli_prepare($dbConnection, "UPDATE stranke SET activeOrNot = 1 WHERE eNaslov = ? AND registerHash = ?");
         mysqli_stmt_bind_param($query, 'ss', $emailUp, $registerHash);
         mysqli_stmt_execute($query);
-        $query = $query -> get_result();
+        $query = $query -> get_result()
 
-        echo 'Opcija 4';
-        //header("Location: ../skupno/prijava.php");
+        echo '<script>alert("Registracija je uspesna, sedaj se lahko prijavite")</script>';
+        header("Location: ../skupno/prijava.php");
       }
     }
+
     else{
-      echo "Opcija 3";
-      //header("Location: ../gosti/registracija.php");
+      echo '<script>alert("Povezava ni veljavna")</script>';
+      header("Location: ../gosti/registracija.php");
     }
 
 }
 else{
-  echo "Opcija 2";
-  //header("Location: ../gosti/registracija.php");
+  echo '<script>alert("Povezava ni veljavna")</script>';
+  header("Location: ../gosti/registracija.php");
 }
