95250c547494e44c89a6eca9199adb65ad24c4cb
mbdev2 <mark.breznik@gmail.com>
2020-12-19 16:55:46 +0100

    Popravki registracije

---
+++ php/zavarovano/gosti/uspesnaRegistracija.php
@@ -0,0 +1,17 @@
+<?php
+	include('../stranke/navigacija.php');
+?>
+
+<html>
+	<head>
+		<title>eSHOP MMA</title>
+	</head>
+	<body>
+		<?php
+			echo $navBarGost;
+		?>
+		<div>
+			<h1>Registracija je bila uspešna, prosimo odprite vas mail in kliknite na potrditveni link. Nato se lahko prijavite. Hvala.<h1/>
+		</div>
+	</body>
+</html>
