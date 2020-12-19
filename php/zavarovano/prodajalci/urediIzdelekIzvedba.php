afa90301a874a07366aacea860c9feea77636e98
mbdev2 <mark.breznik@gmail.com>
2020-12-19 15:53:34 +0100

    Delujoce slike

--- php/zavarovano/prodajalci/urediIzdelekIzvedba.php
+++ php/zavarovano/prodajalci/urediIzdelekIzvedba.php
@@ -23,21 +23,21 @@
 
 	if(isset($_POST['base64slika'])){
 		$base64slika = $_POST['base64slika'];
-		$query = mysqli_prepare($dbConnection, "UPDATE artikli SET slika1=? WHERE idArtikla = ?");
+		$query = mysqli_prepare($dbConnection, "UPDATE artikli SET slika1=? WHERE idArtikla=?");
 		mysqli_stmt_bind_param($query, 'si', $base64slika, $id);
 		mysqli_stmt_execute($query);
 		$query = $query->get_result();
 	}
 	if(isset($_POST['base64slika2'])){
 		$base64slika2 = $_POST['base64slika2'];
-		$query = mysqli_prepare($dbConnection, "UPDATE artikli SET slika2=? WHERE idArtikla = ?");
+		$query = mysqli_prepare($dbConnection, "UPDATE artikli SET slika2=? WHERE idArtikla=?");
 		mysqli_stmt_bind_param($query, 'si', $base64slika2, $id);
 		mysqli_stmt_execute($query);
 		$query = $query->get_result();
 	}
 	if(isset($_POST['base64slika3'])){
 		$base64slika3 = $_POST['base64slika3'];
-		$query = mysqli_prepare($dbConnection, "UPDATE artikli SET slika3=? WHERE idArtikla = ?");
+		$query = mysqli_prepare($dbConnection, "UPDATE artikli SET slika3=? WHERE idArtikla=?");
 		mysqli_stmt_bind_param($query, 'si', $base64slika3, $id);
 		mysqli_stmt_execute($query);
 		$query = $query->get_result();
