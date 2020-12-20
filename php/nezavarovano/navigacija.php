91f64b8da736fcfad3396cd5ece1ddef0da73ec1
Amadej Pavsic <amadej.pavsic@gmail.com>
2020-12-20 20:12:46 +0100

    bootstrap 4.4.1

--- php/nezavarovano/navigacija.php
+++ php/nezavarovano/navigacija.php
@@ -1,8 +1,8 @@
 <?php
-	include('konfiguracija.php');
-	session_start();
+include('konfiguracija.php');
+session_start();
 
-	$navBarGost =
+$navBarGost =
 	'<nav class="navbar navbar-expand-lg navbar-custom">
 		<div class="container-fluid">
 			<a class="navbar-brand" >eSHOPmma</a>
@@ -27,36 +27,36 @@
 		   </div>
 		 </nav>';
 
-	if(isset($_GET['link'])){
-		$link = $_GET['link'];
-	    if ($link == 'domaca') {
-	        $uri = "/gosti/domaca.php";
-			$redirectURL = 'https://' . $_SERVER['HTTP_HOST'] . $uri;
-	    	header('Location: ' . $redirectURL);
-	    }
-	    if ($link == 'registracija'){
-	        $uri = "/gosti/registracija.php";
-			$redirectURL = 'https://' . $_SERVER['HTTP_HOST'] . $uri;
-	    	header('Location: ' . $redirectURL);
-	    }
-	    if ($link == 'prijava'){
-	        $uri = "/stranke/prijava.php";
-			$redirectURL = 'https://' . $_SERVER['HTTP_HOST'] . $uri;
-	    	header('Location: ' . $redirectURL);
-	    }
-	    if ($link == 'prijavaOsebja'){
-	        $uri = "/skupno/prijavaOsebja.php";
-			$redirectURL = 'https://' . $_SERVER['HTTP_HOST'] . $uri;
-	    	header('Location: ' . $redirectURL);
-	    }
+if (isset($_GET['link'])) {
+	$link = $_GET['link'];
+	if ($link == 'domaca') {
+		$uri = "/gosti/domaca.php";
+		$redirectURL = 'https://' . $_SERVER['HTTP_HOST'] . $uri;
+		header('Location: ' . $redirectURL);
 	}
+	if ($link == 'registracija') {
+		$uri = "/gosti/registracija.php";
+		$redirectURL = 'https://' . $_SERVER['HTTP_HOST'] . $uri;
+		header('Location: ' . $redirectURL);
+	}
+	if ($link == 'prijava') {
+		$uri = "/stranke/prijava.php";
+		$redirectURL = 'https://' . $_SERVER['HTTP_HOST'] . $uri;
+		header('Location: ' . $redirectURL);
+	}
+	if ($link == 'prijavaOsebja') {
+		$uri = "/skupno/prijavaOsebja.php";
+		$redirectURL = 'https://' . $_SERVER['HTTP_HOST'] . $uri;
+		header('Location: ' . $redirectURL);
+	}
+}
 
 ?>
 
 <html>
 
 <head>
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 	<link href="../css/domaca.css" rel="stylesheet">
 	<link href="../css/navbar-style.css" rel="stylesheet">
 	<link href="../css/common.css" rel="stylesheet">
@@ -64,8 +64,9 @@
 </head>
 
 <body>
-	<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
+	<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 </body>
 
 </html>
--- php/zavarovano/skupno/navigacija.php
+++ php/zavarovano/skupno/navigacija.php
@@ -125,7 +125,7 @@ $navBarPrijava =
 <html>
 
 <head>
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 	<link href="../css/domaca.css" rel="stylesheet">
 	<link href="../css/navbar-style.css" rel="stylesheet">
 	<link href="../css/common.css" rel="stylesheet">
@@ -135,8 +135,9 @@ $navBarPrijava =
 </head>
 
 <body>
-	<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
+	<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 </body>
 
 </html>
