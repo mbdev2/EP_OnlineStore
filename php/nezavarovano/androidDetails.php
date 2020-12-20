7f268d7346b834f6c4998b286fc535306e704271
MBevc <dblsiti@gmail.com>
2020-12-20 14:31:04 +0100

    added androidDetails

---
+++ php/nezavarovano/androidDetails.php
@@ -0,0 +1,18 @@
+
+<?php
+	
+	include('navigacija.php');
+	if (isset($_GET["idArtikla"])) {
+    	$id = $_GET["idArtikla"];
+}
+
+	$query = mysqli_prepare($dbConnection, "SELECT * FROM artikli WHERE activeOrNot = '1' AND idArtikla=?");
+	mysqli_stmt_bind_param($query, 'i', $id);
+	mysqli_stmt_execute($query);
+	$query = $query->get_result();
+	$curItem = mysqli_fetch_array($query);
+	header('Content-Type: application/json');
+	echo json_encode($curItem, JSON_UNESCAPED_UNICODE);
+
+
+?>
