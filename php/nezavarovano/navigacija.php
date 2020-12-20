3797609fc29dcfba9b685122bc7bd53bc32c782b
Amadej Pavsic <amadej.pavsic@gmail.com>
2020-12-20 17:59:20 +0100

    nezavarovano css

--- php/nezavarovano/navigacija.php
+++ php/nezavarovano/navigacija.php
@@ -57,10 +57,9 @@
 
 <head>
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-	<link href="../zavarovano/css/domaca.css" rel="stylesheet">
-	<link href="../zavaro
-vano/css/navbar-style.css" rel="stylesheet">
-	<link href="../zavarovano/css/common.css" rel="stylesheet">
+	<link href="../css/domaca.css" rel="stylesheet">
+	<link href="../css/navbar-style.css" rel="stylesheet">
+	<link href="../css/common.css" rel="stylesheet">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous" />
 </head>
 
