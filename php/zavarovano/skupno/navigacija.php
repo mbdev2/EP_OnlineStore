8883b6d68b1a392c42776e4c15cd7799f87952d1
Amadej Pavsic <amadej.pavsic@gmail.com>
2020-12-20 20:25:10 +0100

    fix 2

--- php/zavarovano/skupno/navigacija.php
+++ php/zavarovano/skupno/navigacija.php
@@ -125,7 +125,7 @@ $navBarPrijava =
 <html>
 
 <head>
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 	<link href="../css/domaca.css" rel="stylesheet">
 	<link href="../css/navbar-style.css" rel="stylesheet">
 	<link href="../css/common.css" rel="stylesheet">
